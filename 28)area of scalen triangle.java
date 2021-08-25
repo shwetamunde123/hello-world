f14ad29a64ee59bd3b84f37b8de567c63f37d476
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 01:32:09 +0530

    Area of scelan triangle

---
+++ 28)area of scalen triangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class AreaOfTrianlge {
+    public static void main(String args[]){
+    	Scanner sc=new Scanner(System.in);	
+    	double side1, side2, ang1, area;
+		System.out.println("Find the area of Scalene Triangle :");
+		System.out.println("-------------------------------------");		
+        System.out.print("\nInput the length of a side  of the triangle : ");
+    	side1=sc.nextDouble();
+        System.out.print("Input the length of another side  of the triangle : ");
+    	side2=sc.nextDouble();
+        System.out.print("Input the angle between these sides of the triangle :");
+    	ang1=sc.nextDouble();	
+		area = (side1 * side2 * Math.sin((Math.PI/180)*ang1))/2;
+        System.out.println("The area of the Scalen Triangle is : "+area);
+    }
+    }
