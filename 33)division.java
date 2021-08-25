a79eba07909e9699c3a1d7639c5e3f2263972ecb
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 02:02:36 +0530

    Division

---
+++ 33)division.java
@@ -0,0 +1,7 @@
+public class Division{
+public static void main(String args[]){
+int num1=45,num2=9;
+int result=num1/num2;
+System.out.println("Division of "+num1+" and "+num2+" is:"+result);
+}
+}
