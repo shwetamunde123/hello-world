cace1ba93bb6bf7d294f8f38710d814066153a66
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 01:59:23 +0530

    Printing the given pattern

---
+++ 39)print the pattern.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+	
+    System.out.println("\n\n Print the following pattern:\n");
+ System.out.println( "--------------------------------\n");
+	 System.out.println( " xxxxx\n");
+	 System.out.println( "x     x       x        x\n");
+	 System.out.println("x             x        x\n");
+	 System.out.println( "x          xxxxxxx  xxxxxxx\n");
+	 System.out.println("x             x        x\n");
+	 System.out.println("x     x       x        x\n");
+	 System.out.println( " xxxxx\n");
+}
+
+	}
+	
