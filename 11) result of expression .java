d999da454809b04c200da181805fd09660c68736
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 01:07:42 +0530

    Result of expression

---
+++ 11) result of expression .java
@@ -0,0 +1,18 @@
+public class Exercise {
+
+
+public static void main(String[] args) { System.out.println("Result of 1st expression is:");
+
+System.out.println(5+ 8 * 6); System.out.println("Result of 2nd expression is :");
+
+System.out.println((55+9) % 9);
+
+System.out.println("Result of 3rd expression is:"); 
+System.out.println(20 +3*5 / 8);
+
+System.out.println("Result of 4th expression is:");
+
+System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+}
+ }
