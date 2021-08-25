0f2c9b7f01f6dad65c0af799d5db016da5fe49fb
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 02:01:14 +0530

    Result of expression

---
+++ 35)result of expression.java
@@ -0,0 +1,7 @@
+public class Exercise{
+ 
+    public static void main(String[] arg) {
+System.out.println("Result of Expression is (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)");
+        System.out.println( (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
