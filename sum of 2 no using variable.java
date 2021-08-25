746950607c3a32e080a935f026ff3f03aee99a57
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 00:44:21 +0530

    Sum of 2 no using variables

---
+++ sum of 2 no using variable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input the first number: ");
+    int num1 = input.nextInt();
+    System.out.print("Input the second number: ");
+    int num2 = input.nextInt();
+    int sum = num1 + num2;
+    System.out.println(" The sum of  "   +num1+ " and "+num2 + " is:"+ sum);
+  }
+    
+}
