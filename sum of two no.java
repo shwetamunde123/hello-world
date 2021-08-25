efd63360a3fc81ea1ac4bacffdcea9752b88bf71
shwetamunde123 <89522061+shwetamunde123@users.noreply.github.com>
2021-08-26 00:38:35 +0530

    Sum of 2 numbers

---
+++ sum of two no.java
@@ -0,0 +1,12 @@
+public class Sum {
+ 
+ public static void main(String[] args) {
+     int a=20;
+     int b=30;
+     int sum= a+b;
+     
+     
+  System.out.println("sum of "+a+ " and "+ b+ " is: "+sum);
+ }
+ 
+}
